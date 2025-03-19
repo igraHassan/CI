f0437198ed29d743b9d41ffcc2cdafa406929b8d
igraHassan <2195653@talnet.nl>
2025-03-19 13:40:20 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -9,3 +9,14 @@ class TestApp(unittest, TestCase):
       if __name__ == "__main__":
 
 unittest.main()
+
+import unittest
+from app import add
+ 
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+ 
+if __name__ == "__main__":
+    unittest.main()
